f67ed3c3de0fa4d2e16e3d52666d92e61714d22e
amakkaraka99 <120911329+amakkaraka99@users.noreply.github.com>
2023-01-11 15:13:46 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# hallo
+#import dependencies
+import streamlit as st
+import pandas as pd
+
+# import data
+d={'Type':['Notebook','DVDs'],'Qty':[2,3],'Price':[100,200]}
+
+df=pd.DataFrame(data=d)
+st.dataframe(df)
